ea69842783d96fa74ae4fca8ce4312c2cbfbde3a
Андрей Мартынов <50194217+Martin937@users.noreply.github.com>
2024-02-20 10:18:55 +0300

    lesson3 task-01

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,9 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+	let colorsFiltered = colors.filter(color => color === 'черный' || color === 'красный' || color === 'желтый').join('-')
+	return colorsFiltered
+}
+
+console.log(createColorString());
+
